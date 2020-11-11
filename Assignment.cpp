f7444cd252d51e25766a680b81c4960bce18cd6a
waad99 <74211445+waad99@users.noreply.github.com>
2020-11-11 16:31:20 +0300

    Add files via upload

--- Assignment.cpp
+++ Assignment.cpp
@@ -1,38 +1,60 @@
-    /*
-  My name : waad
-  Class : second 
-  Department : computer
-    
-    */
-#include <iostream>
-#include <string>
+/*waad osama
+second
+computer
+*/
 
-int main()
+#include<iostream>
+using namespace std;
+int main(){
+int grade, id_number;                                      // definition the input(grade,id_number,name,department)                                            
+string name, department;
+
+for(int i=1;i<=5 ;i=i+1)
 {
+cout <<"inter your name"<<endl;                          //input the student information
+cin >>name;
+cout <<"inter your id_number"<<endl;
+cin >>id_number;
+cout <<"inter your department"<<endl;
+cin >>department;
+cout <<"inter your grade"<<endl;
+cin >>grade;
 
-      std::string name;
-          std::string degry;
-    std::string input;
-  std::cout << "enter name ";
-  getline (std::cin, name);
-     std::cout << "enter name" << std::endl;
-        std::cin >> input;
-        if( input == "A" )
-        {
-           std::cout << "in control ";
-        }
-        else if( input == "T" )
-        {
-              std::cout << "in communications";
-        }
-           else if( input == "c" )
-        {
-              std::cout << " in computer";
-        }
-        else
-        {
-            std::cout << "The entrance section is not one of sections that belong to the university: " << input << std::endl;
-        }
-        return 0;
-        }
-    
+cout <<"inter your name"<< name << endl;
+cout <<"inter your id_number"<< id_number <<endl;
+cout <<"inter your grade"<< grade <<endl;
+
+
+if ("department == A")                            //if for selection the department
+{
+    cout <<"you are in control department"<<endl;
+} 
+if ("department == T")
+{
+    cout <<"you are in communication department"<<endl;
+}
+if ("departmant == C")
+{
+    cout <<"you are in computer department"<<endl;
+}
+
+
+switch (grade)                                  //switch for selection the grade
+{
+case 50 ... 59:
+cout <<"your grade = f"<<endl;
+case 60 ... 69:
+cout <<"your grade = d"<<endl;
+case 70 ... 79:
+cout <<"your grade = c"<<endl;
+case 80 ... 89:
+cout <<"your grade = b"<<endl;
+case 90 ... 100:
+cout <<"your grade = a"<<endl;
+default :
+cout <<"you are falid"<<endl;
+}
+}
+return 0;
+
+}
