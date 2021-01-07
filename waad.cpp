c9d940f2b3fa428c16584796336674efa19ac971
waad99 <74211445+waad99@users.noreply.github.com>
2021-01-07 03:44:35 -0500

    Add files via upload

---
+++ waad.cpp
@@ -0,0 +1,175 @@
+\\ name : waad osama
+\\class :second
+\\ Department :computer
+# include <iostream >
+using name space std ;
+class stack {
+    sturct  record 
+}
+str name [];
+int num;
+char gmail;
+void push ()
+void pop()
+void search ()
+void edit()
+void incorporate()
+void push ()
+{
+    print ("Build The table ");
+    cout<< endln;
+    cout <"maximum number of  int  100"<<endln ;
+    cout<<"Enter The Following Items"<<endln;
+    for (int i=0; i<=num -1; i++)
+    {
+        cout <<"name ";
+        cin >> name;
+        cout<<"gmail";
+        cin >>gmail;
+
+    }
+}
+void pop()
+{
+    str name;
+    int num;
+    print ("Delete an Entry");
+    cout<< endln;
+    cout<<"Enter an name to Delete that Entry";
+    cin >> name;
+    int i;
+    for  (i=0;i<=mum-1; i++)
+    {
+        if([i].name==name)
+        {
+            num=i;
+        }
+    }
+}
+for (i=0; i<=num -1 i++)
+}
+if(i== num)
+{
+    cout <<"name";
+    cin >> name;
+    cout << "num";
+    cin >> name;
+
+
+}    
+    void edit ()
+    int jobcode;
+    print("Edid an Entry");
+    cout<<endln;
+    int i;
+    void  edit name ();
+    void edit gmail ();
+    viod edit num();
+    char option;
+    cout<<"Enter an jobcode to Edit an Entry...";
+    cin>> jobcode;
+    edit menu();
+    for(i=0; i<=num-1;i++)
+    {
+        if([1]. code == jobcode)
+    }
+switch (option)
+{
+case 'n':
+    editname(i);
+    break;
+case 'c':
+edit gmail (i);
+    break;
+    case 'd';
+    editnum(i);
+    break;
+}
+editmenu();
+}
+}
+}
+}
+void editmenu()
+{
+    cout<<"name";
+    cout<<"gmail";
+    cout<<"num";
+    cout<<"options please";
+}
+void editname(int i);
+{
+    cout<<"Enter New name";
+    cin >> name;
+}
+void editgmail (int i)
+{
+    cout<<"Enter New gmail";
+    cin>> gmail;
+}
+void editnum(int i)
+{
+    cout <<"Enter New num";
+    cin >> num;
+}
+void search()
+{
+    print ("welcome to search of Data base");
+    cout<<endl;
+    int jobcode;
+    cout<<"you can search only By jobcode of an sideconnection";
+    cout<<"Enter code of and sidecon";
+    cin>> jobcode;
+    for (int i=0; i<=num-1; i++)
+    {
+        if([i]. code ==jobcode)
+        {
+            cout<<"Name gmail  num";
+            cout<<setw(1)<< nam;
+            cout<<(2)<< gmail;
+            cout<<(3)<< num;
+            cout<<endl;
+        }
+    }
+}
+cout<<"going to main menu";
+}
+void sort()
+{
+    print("sort The dadabase By jobcode");
+    void sortmenu();
+    void sortname();
+    void sortgmail();
+    void sortnum();
+    char option;
+    
+}
+void sortmenu();
+cout<<endl;
+sortmenu();
+switch(option)
+{
+    case 'n':
+    sortgmail();
+    break;
+}
+case 'c':
+sortgmail();
+break;
+case 'd':
+sortnum();
+break;
+}
+sortmenu();
+}
+}
+void sortmenu()
+{
+    cout<<"name";
+    cin>> name;
+    cout<<"gmail";
+    cin>> gmail;
+    cout<< "num";
+    cin>>num;
+}
+}
